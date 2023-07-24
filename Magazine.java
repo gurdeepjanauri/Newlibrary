14938c95d5b215a473249aff5baaf5bf2588cd91
gurdeepjanauri <124796732+gurdeepjanauri@users.noreply.github.com>
2023-07-24 14:10:11 -0400

    Add files via upload

--- Magazine.java
+++ Magazine.java
@@ -1,14 +1,20 @@
 public class Magazine extends Book {
     private String issue;
+    private String publisher;
 
-    public Magazine(String title, String author, int yearPublished, String issue) {
+    public Magazine(String title, String author, int yearPublished, String issue, String publisher) {
         super(title, author, yearPublished);
         this.issue = issue;
+        this.publisher = publisher;
     }
 
     @Override
     public void displayDetails() {
         super.displayDetails();
         System.out.println("Issue: " + issue);
+        System.out.println("Publisher: " + publisher);
+    
+    
     }
-}
+    }
+
